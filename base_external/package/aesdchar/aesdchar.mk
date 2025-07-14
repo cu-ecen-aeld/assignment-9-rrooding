29394c3b051cc980c155075bac873e98065a7664
Ralph Rooding <ralph@ralphrooding.com>
2025-07-14 10:24:54 +0200

    update hash

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = '7334324'
+AESDCHAR_VERSION = '9bd73d5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
